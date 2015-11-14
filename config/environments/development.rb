e300e5194f00627d3cb139b1aafe47426c07674c
snowacat <snowacat@gmail.com>
2015-11-14 12:41:43 +0300

    initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,4 +14,10 @@ NewsKeeper::Application.configure do
   ENV['SECRET_TOKEN'] = '02048945f67ded6fda8747ea6405c1c069518c64e0412b8d9e1cdf0434c1aa11be7d3dec3e709deb4b7b8cdbe72319c9f0d3ad0761317c2a6f9062115bed54c5'
   ENV['RE_CAPTCHA_PUBLIC'] = '6LdvowYTAAAAACvEEQOHbN_6kLOh8pR5HXcf6hn1'
   ENV['RE_CAPTCHA_PRIVATE'] = '6LdvowYTAAAAACFJvKKEUyrU-H_2ZwCIs650JG_p'
+
+  ENV['DB_HOST'] = 'localhost'
+  ENV['DB_USERNAME'] = 'postgres'
+  ENV['DB_PASSWORD'] = 'student'
+  ENV['DB_DATABASE'] = 'diplom'
+  ENV['DB_PORT'] = '5432'
 end
